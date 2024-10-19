ab63ca9e5940b5d67575f45f12eded48a8727461
Nick-Lemy <n.kayiranga@alustudent.com>
2024-10-19 13:58:21 +0200

    PEP8 validation error fixed hopefully

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -6,19 +6,25 @@ import requests
 
 if __name__ == '__main__':
     """gather data from an api"""
-    response_todos = requests.get("https://jsonplaceholder.typicode.com/user/"+sys.argv[1]+"/todos")
+    response_todos = requests.get(
+        "https://jsonplaceholder.typicode.com/user/" + sys.argv[1] + "/todos"
+    )
     data = response_todos.json()
     completed_tasks = []
     for i in data:
         if i['completed']:
-          completed_tasks.append(i['title'])
+            completed_tasks.append(i['title'])
 
-    user = requests.get("https://jsonplaceholder.typicode.com/users/"+sys.argv[1]).json()
+    user = requests.get(
+        "https://jsonplaceholder.typicode.com/users/" + sys.argv[1]
+    ).json()
     tasks = ""
     for i in range(0, len(completed_tasks)):
-        if i == len(completed_tasks) -1:
-           tasks += "\t" + completed_tasks[i]
+        if i == len(completed_tasks) - 1:
+            tasks += "\t" + completed_tasks[i]
         else:
             tasks += "\t" + completed_tasks[i] + "\n"
-    print(f"Employee {user['name']} is done with tasks({len(completed_tasks)}/{len(data)}):")
+    print(
+        f"Employee {user['name']} is done with tasks({len(completed_tasks)}/{len(data)}):"
+    )
     print(tasks)
