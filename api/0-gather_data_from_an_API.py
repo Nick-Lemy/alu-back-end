57ce6033e239461f7e8f6778b15c4b051addf909
Nick-Lemy <n.kayiranga@alustudent.com>
2024-10-19 13:47:45 +0200

    task 0

---
+++ api/0-gather_data_from_an_API.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+import sys
+import requests
+
+response_todos = requests.get("https://jsonplaceholder.typicode.com/user/"+sys.argv[1]+"/todos")
+data = response_todos.json()
+completed_tasks = []
+for i in data:
+    if i['completed']:
+        completed_tasks.append(i['title'])
+
+user = requests.get("https://jsonplaceholder.typicode.com/users/"+sys.argv[1]).json()
+tasks = ""
+for i in range(0, len(completed_tasks)):
+    if i == len(completed_tasks) -1:
+        tasks += "\t" + completed_tasks[i]
+    else:
+        tasks += "\t" + completed_tasks[i] + "\n"
+print(f"Employee {user['name']} is done with tasks({len(completed_tasks)}/{len(data)}):")
+print(tasks)
